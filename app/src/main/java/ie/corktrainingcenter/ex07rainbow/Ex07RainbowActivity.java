c1609508945ce69dbfb2970444fc2fdee3e5961a
AppDeveloper <AppDeveloper>
2017-04-20 10:11:13 +0100

    Initial commit

---
+++ app/src/main/java/ie/corktrainingcenter/ex07rainbow/Ex07RainbowActivity.java
@@ -0,0 +1,13 @@
+package ie.corktrainingcenter.ex07rainbow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ex07RainbowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex07_rainbow);
+    }
+}
